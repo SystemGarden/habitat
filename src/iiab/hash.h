a168b90327f7045f253ac92c53cf45403e92d9d3
Nigel Stuckey <nigel@systemgarden.com>
2011-09-15 19:40:37 +0000

    moved 1.x files into a branch for long term maintenance

--- src/iiab/hash.h
+++
@@ -1,8 +0,0 @@
-/*
- * Simple hash algroithm
- * taken from http://burtleburtle.net/bob/hash/doobs.html
- */
-
-/* functional prototypes */
-unsigned int hash_block(char *k, unsigned int length, unsigned int initval);
-unsigned int hash_str(char *str);
